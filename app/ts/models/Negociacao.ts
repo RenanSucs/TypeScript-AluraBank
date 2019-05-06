8dba2e5b55b39b382fb76e27f3b386ebfb51b964
RenanSucs <47055395+RenanSucs@users.noreply.github.com>
2019-05-06 20:42:14 -0300

    commit

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,24 +1,10 @@
 export class Negociacao {
 
-    constructor(private _data: Date, private _quantidade: number, private _valor: number){}
+    constructor(readonly data: Date, readonly quantidade: number, readonly valor: number){}
                 //declara como privada e define o tipo da variavel: Date, number, string
-    get data(){
-
-        return this._data;
-    }
-
-    get quantidade(){
-
-        return this._quantidade;
-    }
-
-    get valor(){
-
-        return this._valor;
-    }
 
     get volume(){
 
-        return this.quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
